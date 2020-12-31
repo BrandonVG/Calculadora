381cc2ddb93401ebf6f4f0d213c17af0418b9703
BrandonVG <54459465+BrandonVG@users.noreply.github.com>
2020-12-30 20:49:50 -0600

    Install

---
+++ install.js
@@ -0,0 +1,5 @@
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+deferredInstallPrompt = evt;
+installButton.removeAttribute('hidden');
+deferredInstallPrompt.prompt();
+evt.srcElement.setAttribute('hidden', true);
